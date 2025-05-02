6f433a04fd0834c272a39fa951d593a8f3740177
An53657 <kell3412@163.com>
2025-05-02 15:39:35 +0800

    add some new filse

--- Foundation/Day05/arrayDeno/arrayDemo02.java
+++ Foundation/Day05/arrayDeno/arrayDemo02.java
@@ -10,9 +10,17 @@ public class arrayDemo02 {
         //获取数组第一位元素
         ///格式:数组名[索引];
         //其实就是0索引对应上的值
-        int number =array [0];
-        System.out.println(number);
+        //int number =array [0];
+        //定一种
+        //System.out.println(number);
+        //第二种
+        //获取数组中1索引上对应的数据，并直接打印出来
+        //System.out.println(array[1]);
 
         //2.把数据存储到数组当中
+        //格式 数组名[索引] = 具体数据/变量
+        //细节:一旦覆盖了，原来的数据就不存在了
+        array[0] = 120;
+        System.out.println( array[0]);
     }
 }

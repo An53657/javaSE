3b12fd306a64f4d072e5475a132089b9cb190b04
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-06-02 10:58:18 +0800

    add some new files

--- MyTest/ObjectOrientedDemoo/CityDemo02/Citytest.java
+++
@@ -1,9 +0,0 @@
-package MyTest.ObjectOrientedDemoo.CityDemo02;
-
-public class Citytest {
-    public static void main(String[] args) {
-        City frist = new City();
-        frist.setName("bejing");
-        System.out.println(frist.getName());
-    }
-}

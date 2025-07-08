6ded252687f74e270c6c8b32c07e0260e05882ea
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-07-08 19:56:10 +0800

    add some files

---
+++ Foundation/Day15/a06interfacedemo06/Interimpl.java
@@ -0,0 +1,16 @@
+package a06interfacedemo06;
+
+public class Interimpl implements Inter1 {
+
+    @Override
+    public void methood() {
+        System.out.println("实现类重写了抽象方法");
+    }
+    // 不能用defult来重写默认方法
+
+    // @Override
+    // public defult void show() {
+    // System.out.println("重写接口的默认方法");
+    // }
+
+}
---
+++ Foundation/Day15/a06interfacedemo06/Test.java
@@ -0,0 +1,10 @@
+package a06interfacedemo06;
+
+public class Test {
+    public static void main(String[] args) {
+        // 创建实现类对象
+        Interimpl ii = new Interimpl();
+        ii.methood();
+        ii.show();
+    }
+}

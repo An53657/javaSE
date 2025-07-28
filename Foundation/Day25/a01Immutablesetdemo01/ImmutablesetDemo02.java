1b7402d5cab977e3d57060d0099f779b89556ed1
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-07-28 10:21:59 +0800

    add some files

--- Foundation/Day25/a01Immutablesetdemo01/ImmutablesetDemo02.java
+++ Foundation/Day25/a01Immutablesetdemo01/ImmutablesetDemo02.java
@@ -6,6 +6,7 @@ import java.util.Set;
 public class ImmutablesetDemo02 {
     public static void main(String[] args) {
         // 创建不可变的set集合
+        //当我们要获取一个不可变的Set集合时，参数要保持唯一
         Set<Integer> set = Set.of(114, 2, 3, 5, 7, 8, 9);
 
         // 添加

7afc86ce817161e6e22171be102cb718d21c3156
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-07-30 16:07:35 +0800

    add some files

---
+++ Foundation/Day25/a03Functiondemo03/Functiondemo01/StringOperation.java
@@ -0,0 +1,7 @@
+package Day25.a03Functiondemo03.Functiondemo01;
+
+public class StringOperation {
+    public boolean StringJude(String s) {
+        return s.startsWith("张") && s.length() == 3;
+    }
+}
---
+++ Foundation/Day25/a03Functiondemo03/Functiondemo01/Stundent.java
@@ -0,0 +1,41 @@
+package Day25.a03Functiondemo03.Functiondemo01;
+
+public class Stundent {
+    private String name;
+    private int age;
+
+    public Stundent() {
+
+    }
+
+    public Stundent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Stundent(String str) {
+        this.name = str.split(",")[0];
+        this.age = Integer.parseInt(str.split(",")[1]);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{name='" + name + "', age=" + age + "}";
+    }
+}

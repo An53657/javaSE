aca17227ec56d21a89e6a62192b5cf9ded3f14ed
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-08-01 13:52:49 +0800

    add some files

--- Foundation/Day25/a03Functiondemo03/Functiondemo01/StringOperation.java
+++
@@ -1,7 +0,0 @@
-package Day25.a03Functiondemo03.Functiondemo01;
-
-public class StringOperation {
-    public boolean StringJude(String s) {
-        return s.startsWith("张") && s.length() == 3;
-    }
-}

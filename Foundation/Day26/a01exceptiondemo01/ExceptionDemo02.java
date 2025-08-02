2369a8f4f14579b0790f6f6dc61fbe61500c54b9
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2025-08-02 14:39:12 +0800

    add some files

--- Foundation/Day26/a01exceptiondemo01/ExceptionDemo02.java
+++ Foundation/Day26/a01exceptiondemo01/ExceptionDemo02.java
@@ -1,14 +1,13 @@
 /*
- - 作用一：异常用于查询bug的关键参考信息
+- 作用一：异常用于查询bug的关键参考信息
 - 作用二：异常可以可以作为方法内部的一种特殊的返回值，以便通知调用者底层的执行情况
  */
 package Day26.a01exceptiondemo01;
 
-public class ExceptionDemo01 {
+public class ExceptionDemo02 {
     public static void main(String[] args) {
         // 作用一：异常用于查询bug的关键参考信息
-        Stundent[] arr = new Stundent[3];
-        String name = arr[0].getName();
-        System.out.println(name);
+        Stundent2 stu = new Stundent2("张三,23");
+        System.out.println(stu);
     }
 }
